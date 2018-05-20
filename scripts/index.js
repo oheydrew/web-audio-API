40d8b6e61b413d0351953463fb2528cf323c7ed2
oheydrew <drew@oheydrew.me>
2018-05-20 19:58:54 +1000

    Add basic button / pitch slider

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 var audioContext;
+const pitchSlider = document.getElementById("pitchSlider"); 
 window.addEventListener('load', initAudioContext, false);
 
 // Initializes an Audio Context object
@@ -15,37 +16,47 @@ function initAudioContext() {
 
 // Load a sound into the AudioContext
 var keyStrokeBuffer = null;
-loadAudio('/keypress.wav')
+// loadAudio('/keypress.wav')
+loadAudio('https://storage.googleapis.com/www.oheydrew.me/stuff/keypress.wav')
 
 function loadAudio(url) {
+  console.log(`Audio Load Request for: ${url}`)
   var request = new XMLHttpRequest();
   request.open('GET', url, true);
-  request.responseType = 'arraybuffer';
-  console.log(request.response)
+  request.responseType = 'arraybuffer'; // 'arraybuffer' is a binary data type, ie audio files
+  request.send(); // Send off the request
+
+  // Console logging the response
+  request.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      console.log(`Completed XMLHttpRequest with Response ${this.readyState}, with Status ${this.status} ${this.statusText}`)
+      console.log(`Response: ${this.response}`)
+    }
+  }
 
   // Decode asynchronously
   request.onload = function() {
-    context.decodeAudioData(request.response, function(buffer) {
-      keyStrokeBuffer = buffer;
-      console.log(`Buffer Loaded: ${url}`)
-    }, onError);
-  }
-  
-  request.onreadystatechange = function() {
-    // if (this.readyState == 4 && this.status == 200) {
-      console.log(`Completed XMLHttpRequest with Response ${this.readyState}, with Status ${this.status} ${this.statusText}`)
-      console.log(`Response: ${this.response}`)
-  }
+    // decodeAudioData takes a: ([file to decode], [completed callback], [error callback]) 
+    audioContext.decodeAudioData(request.response, onDecodeSuccess, onDecodeError);
 
-  request.send();
+    function onDecodeError(error) {
+      console.log(`Decode Error: ${error}`)
+    }
+    function onDecodeSuccess(buffer) {
+        keyStrokeBuffer = buffer;
+        console.log(`Decode Success: Buffer Loaded`)
+    }
   }
-  
+}
+
 function playSound(buffer) {
   // creates a sound source
-  var source = context.createBufferSource(); 
+  console.log('playSound called')
+  var source = audioContext.createBufferSource(); 
   // tell the source which sound to play
   source.buffer = buffer;                    
   // connect the source to the context's destination (the speakers)
-  source.connect(context.destination);       
+  source.connect(audioContext.destination);    
+  source.detune.value = pitchSlider.value  
   source.start(0);                           // play the source now
 }
